9af24e66768251716f641d61d303ebfdfac8c641
Save the World now <worldpeacebtn@gmail.com>
2025-11-08 16:47:26 +0100

    Update project titles in constants/index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -259,21 +259,21 @@ export const OTHER_SKILL = [
 
 export const PROJECTS = [
   {
-    title: "Modern Next.js 14 Portfolio",
+    title: "Universal Declaration of Human Rights",
     description:
-      'Embark on a journey through my professional evolution with the "Modern Next.js Portfolio" - a dynamic showcase of my skills, experiences, and passion for web development. Crafted with precision and powered by Next.js, this portfolio is more than just a static display; it\'s an immersive experience that reflects the cutting edge of modern web technologies.',
-    image: "/projects/project-1.png",
-    link: "https://example.com",
+      'We, the rightful  demand our rights back. Everyone unites under "ACAB" [All Citizens Are Besties] principle. ',
+    image: "/projects/HumanRightsLogo.svg.png",
+    link: "https://www.un.org/en/about-us/universal-declaration-of-human-rights",
   },
   {
-    title: "Interactive Cards Portfolio",
+    title: "CASE X42",
     description:
       'Step into the extraordinary world of my professional journey through the "Interactive Cards Portfolio" - an innovative and visually captivating platform that redefines the traditional portfolio experience. Ditching the conventional static layout, this portfolio leverages interactive cards to showcase my skills, projects, and personality in an engaging and dynamic manner.',
     image: "/projects/project-2.png",
     link: "https://example.com",
   },
   {
-    title: "Space Themed Website",
+    title: "CASE X43",
     description:
       'Embark on an interstellar journey with my "Space Themed Website", a mesmerizing space-themed website that invites you to explore the cosmic wonders beyond our world. Immerse yourself in an awe-inspiring digital experience that blends cutting-edge design with the mysteries of the universe.',
     image: "/projects/project-3.png",
