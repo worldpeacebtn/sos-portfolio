6c8d1d16aa755774164a8b54fb05fe03c4c73e05
Save the World now <worldpeacebtn@gmail.com>
2025-11-24 20:50:11 +0100

    Update hero.tsx

--- components/main/hero.tsx
+++ components/main/hero.tsx
@@ -2,14 +2,17 @@ import { HeroContent } from "@/components/sub/hero-content";
 
 export const Hero = () => {
   return (
-    <div className="relative flex flex-col h-full w-full">
+    <div className="relative h-screen w-full overflow-hidden">
       <video
         autoPlay
         muted
         loop
-        className="rotate-180 absolute top-[-25%] left-0 w-full h-full object-cover -z-20"
+        playsInline
+        preload="auto"
+        className="absolute top-0 left-0 w-full h-full object-cover -z-20"
       >
         <source src="/videos/blackhole.webm" type="video/webm" />
+        <source src="/videos/blackhole.mp4" type="video/mp4" />
       </video>
 
       <HeroContent />
