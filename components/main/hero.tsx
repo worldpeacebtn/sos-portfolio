a222da7b21061ce63162f2d5da54273bf3f8fb76
Save the World now <worldpeacebtn@gmail.com>
2025-11-24 20:46:31 +0100

    Update hero.tsx

--- components/main/hero.tsx
+++ components/main/hero.tsx
@@ -7,7 +7,6 @@ export const Hero = () => {
         autoPlay
         muted
         loop
-        playsInline
         className="rotate-180 absolute top-[-25%] left-0 w-full h-full object-cover -z-20"
       >
         <source src="/videos/blackhole.webm" type="video/webm" />
