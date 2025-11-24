d1ef820e2b8e0071db1f8d4334b1766607704395
Save the World now <worldpeacebtn@gmail.com>
2025-11-24 21:03:29 +0100

    Update star-background.tsx

--- components/main/star-background.tsx
+++ components/main/star-background.tsx
@@ -41,7 +41,7 @@ export const StarBackground = (props: PointsProps) => {
 };
 
 export const StarsCanvas = () => (
-  <div className="w-full h-auto fixed inset-0 -z-10">
+  <div className="pointer-events-none fixed inset-0 -z-30 w-full h-full">
     <Canvas camera={{ position: [0, 0, 1] }}>
       <Suspense fallback={null}>
         <StarBackground />
