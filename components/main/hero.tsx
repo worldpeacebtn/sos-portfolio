deadbc018a7a9be97b1dd4d0e5f9f79290ab79bc
Save the World now <worldpeacebtn@gmail.com>
2025-11-24 21:08:16 +0100

    Update hero.tsx

--- components/main/hero.tsx
+++ components/main/hero.tsx
@@ -4,13 +4,13 @@ export const Hero = () => {
   return (
 <div className="relative w-full h-screen overflow-visible">
       <video
-        autoPlay
-        muted
-        loop
-        playsInline
-        preload="auto"
-        className="absolute top-0 left-0 w-full h-full object-cover -z-20"
-      >
+  autoPlay
+  muted
+  loop
+  playsInline
+  preload="auto"
+  className="absolute top-0 left-0 w-full h-full object-cover -z-20 border-4 border-red-500"
+>
         <source src="/videos/blackhole.webm" type="video/webm" />
         <source src="/videos/blackhole.mp4" type="video/mp4" />
       </video>
