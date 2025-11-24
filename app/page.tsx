82941f0c160045e60e7d3935937407952353216b
Save the World now <worldpeacebtn@gmail.com>
2025-11-24 21:11:06 +0100

    Update page.tsxJa

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,11 @@ export default function Home() {
   return (
     <main className="h-full w-full">
       <div className="flex flex-col gap-20">
+<section className="relative w-full h-screen overflow-hidden m-0 p-0">
+  <Hero />
+
         <Hero />
+  </section>
         <Skills />
         <Encryption />
         <Projects />
